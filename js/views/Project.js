4ebf5d7753d187840edfb99f47c60c238364221b
Vishaal Kalwani <vishaalk@umich.edu>
2014-05-21 22:42:23 -0700

    Made my first view

---
+++ js/views/Project.js
@@ -0,0 +1,18 @@
+var ProjectView = Backbone.View.extend({
+	initialize: function() {
+		this.render();
+		alert('yo');
+	},
+
+	render: function() {
+		//second value is an options hash
+		var template = _.template( $("#ProjectTemplate").html(), {});
+       	// Load the compiled HTML into the Backbone "el"
+       	// all views come with an $el
+       	// defaulted to an empty div
+       	// so we must bind it when we initialize it
+        this.$el.html( template );
+	}
+});
+
+var projectView = new ProjectView({ el :  $("#ProjectContainer") });
---
+++ js/views/User.js
@@ -0,0 +1,7 @@
+var UserView = Backbone.View.extend({
+    initialize: function() {
+        alert("Hello there!");
+    }
+});
+
+var userView = new UserView();
